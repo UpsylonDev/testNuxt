cd7d337a95d2063694215e8f85d69839f68b8db5
Yann Mingant <yann.mingant@gmail.com>
2020-09-13 12:26:48 +0200

    Ajout page about

---
+++ testnuxtjs/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="fond">
+        <h1>Test page about </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.fond{
+    width: 80%;
+    margin-left: 20px;
+    margin-top: 2%;
+    background: rgba(167, 166, 166, 0.479);
+}
+
+</style>
