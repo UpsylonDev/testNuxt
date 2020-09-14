2d4999e1013774e44013a1444b14df668bff2aaf
Upsy <upsylon.dev@gmail.com>
2020-09-14 09:26:49 +0200

    ajout lien aide issues instal uikit

--- testnuxtjs/plugins/uikit.js
+++ testnuxtjs/plugins/uikit.js
@@ -1,6 +1,8 @@
+/**
+ * Install config UIkit depuis l'issue :
+ * https://github.com/uikit/uikit/issues/3706
+ */
 import Vue from 'vue'
-
-// import UIkit from 'uikit'
 import UIkit from 'uikit/dist/js/uikit.js'
 import UIkitss from 'uikit/dist/css/uikit.css'
 import Icons from 'uikit/dist/js/uikit-icons'
